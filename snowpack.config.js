97c4f163dd686235d141954d7a9f6b9b720fd463
Ryan Jansen <rjgiga444@gmail.com>
2021-05-15 17:28:10 +0800

    Created repo

--- snowpack.config.js
+++ snowpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
         ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
       },
     ],
+    '@snowpack/plugin-sass'
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
